5752d45d0a49d0e1f724d5ae6dc92996ca1471af
Anita Thamrin <anita.thamrin@gmail.com>
2025-04-26 17:24:50 +0800

    done my assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import photo from "./assets/photo.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <img style={{marginTop:"10px"}} src={photo} width="200px" height="250px" alt=""/>
+      <h1>Anita Thamrin</h1>
+      <h2>Operational Support Analyst</h2>
+      <div style={{border:"1px solid black", marginRight:"300px", marginLeft:"300px"}}>
+        <h3>About me</h3>
+        <hr style={{margin:"auto 200px", border:"1px solid white", boxShadow:"0px 2px 2px rgba(0, 0, 0, 0.3)"}} />
+        <p style={{textAlign:"center"}}>I am a dedicated and experienced employee with 14 years of working experience.
+          Currently, I am attending Software Engineering course in the pursuit of becoming 
+          a software developer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div style={{border:"1px solid black", marginRight:"300px", marginLeft:"300px", marginTop:"10px"}}>
+        <h3>Contact me</h3>
+        <hr style={{margin:"auto 200px", border:"1px solid white", boxShadow:"0px 2px 2px rgba(0, 0, 0, 0.3)"}} />
+        <p style={{textAlign:"center"}}>
+          Email: anita.thamrin@gmail.com | LinkedIn: https://www.linkedin.com/in/anita-thamrin/
+        </p>
+      </div>
+      
     </div>
   );
 }
